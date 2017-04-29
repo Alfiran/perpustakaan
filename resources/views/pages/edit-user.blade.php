48a8bbca51bf786900d0a3e77b483ce24ed5009d
Billy Everyteen <fadilahalfira@gmail.com>
2017-04-29 09:35:36 +0700

    change edit user

--- resources/views/pages/edit-user.blade.php
+++ resources/views/pages/edit-user.blade.php
@@ -79,26 +79,13 @@
 
 @endsection
 @section('scripts')
-<!--   Core JS Files   -->
-<script src={{asset('assets/js/jquery-1.10.2.js')}} type="text/javascript"></script>
-<script src={{asset('assets/js/bootstrap.min.js')}} type="text/javascript"></script>
-
-<!--  Checkbox, Radio & Switch Plugins -->
-<script src={{asset('assets/js/bootstrap-checkbox-radio-switch.js')}}></script>
-
-<!--  Charts Plugin -->
-<script src={{asset('assets/js/chartist.min.js')}}></script>
-
-<!--  Notifications Plugin    -->
-<script src={{asset('assets/js/bootstrap-notify.js')}}></script>
-
-<!--  Google Maps Plugin    -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
-
-<!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
-<script src={{asset('assets/js/light-bootstrap-dashboard.js')}}></script>
-
-<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
-<script src={{asset('assets/js/demo.js')}}></script>
-
+<script>
+  $(document).ready(function(){
+    // aktifkan class nav user
+    $('#nav-dashboard').removeClass('active');
+    $('#nav-list-transaksi').removeClass('active');
+    $('#nav-list-book').removeClass('active');
+    $('#nav-list-user').addClass('active');
+  });
+</script>
 @endsection
