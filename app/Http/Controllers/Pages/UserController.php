a4d8984c6dfed23b6c0f084ee0944774cb9aa421
Billy Everyteen <fadilahalfira@gmail.com>
2017-04-29 09:34:45 +0700

    passing data to view

---
+++ app/Http/Controllers/Pages/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use App\Http\Requests\User\UserCreateRequest;
+use Illuminate\Http\Request;
+use App\Domain\Repositories\UserRepository;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+
+    /**
+     * @var UserInterface
+     */
+    protected $user;
+
+    /**
+     * UserController constructor.
+     * @param UserInterface $user
+     */
+    public function __construct(UserRepository $user)
+    {
+        $this->user = $user;
+    }
+
+    public function index(Request $request)
+    {
+        $users = $this->user->paginate(10, $request->input('name'), $column = ['*'], '', $request->input('search'));
+        return view('pages.list-user', ['users' => $users]); 
+    }
+
+    public function create()
+    {
+        return view('pages.create-user'); 
+    }
+
+    public function edit($id)
+    {
+        return view('pages.edit-user'); 
+    }
+
+}
--- resources/views/pages/list-user.blade.php
+++ resources/views/pages/list-user.blade.php
@@ -5,7 +5,6 @@
           <div class="row">
             <div class="col-md-12">
               <div class="card">
-
                 <div class="header">
                   <h4 class="title">List User</h4>
                 </div>
@@ -13,7 +12,7 @@
                   <div class="row">
                     <div class="col-md-8 pull-left">
                       <div class="font-icon-list">
-                        <a class="btn btn-default submit" href="create-user">
+                        <a class="btn btn-default submit" href={{route('page.create-user')}}>
                           <i class="pe-7s-plus"></i>
                         </a>
                         <button class="btn btn-default submit">
@@ -30,7 +29,6 @@
                   <div class="table-responsive table-full-width">
                     <table class="table table-hover table-striped">
                       <thead>
-                        <th>Id</th>
                         <th>Nama</th>
                         <th>Kelas</th>
                         <th>Alamat</th>
@@ -38,105 +36,25 @@
                         <th>Level</th>
                       </thead>
                       <tbody>
-                        <tr>
-                          <td>001</td>
-                          <td>Alfira</td>
-                          <td>XI RPL 2</td>
-                          <td>Kepanjen</td>
-                          <td>098657245378</td>
-                          <td>Siswa</td>
-                          <td>
-                           <a class="btn btn-default" href="edit-user">
-                              <i class="pe-7s-pen"></i>
-                            </a>
-                            <button class="btn btn-danger">
-                              <i class="pe-7s-trash"></i>
-                            </button>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>002</td>
-                          <td>Alfira</td>
-                          <td>XI RPL 2</td>
-                          <td>Kepanjen</td>
-                          <td>098765432987</td>
-                          <td>Siswa</td>
-                          <td>
-                            <a class="btn btn-default" href="edit-user">
-                              <i class="pe-7s-pen"></i>
-                            </a>
-                            <button class="btn btn-danger">
-                              <i class="pe-7s-trash"></i>
-                            </button>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>003</td>
-                          <td>Alfira</td>
-                          <td>XI RPL 2</td>
-                          <td>Kepanjen</td>
-                          <td>098765432987</td>
-                          <td>Siswa</td>
-                          <td>
-                           <a class="btn btn-default" href="edit-user">
-                              <i class="pe-7s-pen"></i>
-                            </a>
-                            <button class="btn btn-danger">
-                              <i class="pe-7s-trash"></i>
-                            </button>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>003</td>
-                          <td>Alfira</td>
-                          <td>XI RPL 2</td>
-                          <td>Kepanjen</td>
-                          <td>098765432987</td>
-                          <td>Siswa</td>
-                          <td>
-                           <a class="btn btn-default" href="edit-user">
-                              <i class="pe-7s-pen"></i>
-                            </a>
-                            <button class="btn btn-danger">
-                              <i class="pe-7s-trash"></i>
-                            </button>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>004</td>
-                          <td>Doris Greene</td>
-                          <td>XI RPL 3</td>
-                          <td>Malang</td>
-                          <td>098765432987</td>
-                          <td>Siswa</td>
-                          <td>
-                           <a class="btn btn-default" href="edit-user">
-                              <i class="pe-7s-pen"></i>
-                            </a>
-                            <button class="btn btn-danger">
-                              <i class="pe-7s-trash"></i>
-                            </button>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>005</td>
-                          <td>Mason Porter</td>
-                          <td>XI RPL 1</td>
-                          <td>Malang</td>
-                          <td>876900856345</td>
-                          <td>Siswa</td>
-                          <td>
-                            <a class="btn btn-default" href="edit-user">
-                              <i class="pe-7s-pen"></i>
-                            </a>
-                            <button class="btn btn-danger">
-                              <i class="pe-7s-trash"></i>
-                            </button>
-                          </td>
-                        </tr>
+                        @foreach ($users as $user)
+                          <tr>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->class }}</td>
+                            <td>{{ $user->address }}</td>
+                            <td>{{ $user->phone }}</td>
+                            <td>{{ $user->level }}</td>
+                            <td>
+                              <a class="btn btn-default" href={{route('page.edit-user',['id' => $user->id])}}>
+                                <i class="pe-7s-pen"></i>
+                              </a>
+                              <button class="btn btn-danger">
+                                <i class="pe-7s-trash"></i>
+                              </button>
+                            </td>
+                          </tr>
+                        @endforeach
                       </tbody>
                     </table>
-
                   </div>
                 </div>
               </div>
@@ -145,25 +63,4 @@
         </div>
 @endsection
 @section('scripts')
-<!--   Core JS Files   -->
-<script src={{asset('assets/js/jquery-1.10.2.js')}} type="text/javascript"></script>
-<script src={{asset('assets/js/bootstrap.min.js')}} type="text/javascript"></script>
-
-<!--  Checkbox, Radio & Switch Plugins -->
-<script src={{asset('assets/js/bootstrap-checkbox-radio-switch.js')}}></script>
-
-<!--  Charts Plugin -->
-<script src={{asset('assets/js/chartist.min.js')}}></script>
-
-<!--  Notifications Plugin    -->
-<script src={{asset('assets/js/bootstrap-notify.js')}}></script>
-
-<!--  Google Maps Plugin    -->
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
-
-<!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
-<script src={{asset('assets/js/light-bootstrap-dashboard.js')}}></script>
-
-<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
-<script src={{asset('assets/js/demo.js')}}></script>
 @endsection
