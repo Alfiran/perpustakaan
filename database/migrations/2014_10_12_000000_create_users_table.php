6622dcd5e100b1190c302052c1b86ab3a9aab888
Edi Santoso <edicyber@gmail.com>
2017-05-06 08:23:38 +0700

    add field email

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('email');
             $table->string('class');
             $table->string('address');
             $table->string('phone');
